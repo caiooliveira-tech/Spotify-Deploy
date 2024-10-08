75a8e83c182194dd3c0add961276e890da3e6a75
Caio Oliveira <caio.oliveira07@gmail.com>
2024-10-08 15:02:08 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import novoArtista from './models/NovoArtistas.js';
 const app = express();
 app.use(cors(
     {
-        origin: 'https://spotify-deploy-front.vercel.app/',
+        origin: 'https://spotify-deploy-front.vercel.app',
         methods: ["POST", "GET"],
         credentials: true
     }
