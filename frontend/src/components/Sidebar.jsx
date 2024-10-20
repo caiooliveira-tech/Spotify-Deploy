9f32e4f4c2ef7cc44d123b5b8636f7541ae2f531
Caio Oliveira <caio.oliveira07@gmail.com>
2024-10-20 03:07:41 -0300

    Sidebar preenchida

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -6,8 +6,13 @@ export default function Sidebar() {
     const { isLogged } = useContext(UserContext);
 
     return(
-        <aside className="bg-gray-900 rounded-lg w-3/12 flex flex-col justify-around items-center">
-          {isLogged && <button className="bg-white py-2 px-4 rounded-full text-gray-900 font-medium hover:scale-105 transition duration-200">Criar Playlist</button> }
+        <aside className="bg-gray-900 rounded-lg w-3/12 flex justify-center items-center">
+          {
+          isLogged ? 
+          <button className="bg-white py-2 px-4 rounded-full text-gray-900 font-medium hover:scale-105 transition duration-200">Criar Playlist</button>
+          :
+          <p className="font-semibold text-center">Faça Login para criar playlists personalizadas!</p>
+         }
         </aside>
     )
 }
