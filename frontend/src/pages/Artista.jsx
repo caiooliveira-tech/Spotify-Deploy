9e0776b328ce5644009b9f3a3efb490b8932c65f
Caio Oliveira <caio.oliveira07@gmail.com>
2024-10-20 03:14:53 -0300

    Loader adicionado na página de artista

--- frontend/src/pages/Artista.jsx
+++ frontend/src/pages/Artista.jsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router"
+import { waveform } from 'ldrs' 
 
 export default function Artista() {
 
+    waveform.register();
+
     const {id} = useParams()
     const [artista, setArtista] = useState({})
+    const [isLoading, setIsLoading] = useState(false);
+
     useEffect(() => {
+        setIsLoading(true);
             const puxarArtista = async () => {
         try{
             const artista = await fetch(`https://spotify-deploy-api.vercel.app/artistas/${id}`);
@@ -15,6 +21,9 @@ export default function Artista() {
         catch (error) {
                 console.error('Error fetching data:', error);
         }
+        finally {
+            setIsLoading(false);
+        }
     }
     puxarArtista();
 
@@ -23,11 +32,25 @@ export default function Artista() {
     
     return (
         <div className="bg-gray-900 rounded-md w-8/12 p-10">
+        {isLoading ?
+          <div className="flex flex-col flex-wrap justify-center items-center w-full font-bold gap-4">
+            <p>Carregando...</p>
+              <l-waveform
+                size="55"
+                stroke="3.5"
+                speed="1"
+                color="white" 
+              ></l-waveform>
+            </div>
+          :
+          <>
             <div className="flex items-center mb-4 gap-5">
                 <img src={artista.capa? artista.capa : '/capa.jpg'} alt="Capa do artista" className="w-[100px] h-[100px]"/>
                 <h1 className="text-7xl font-bold">{artista.name}</h1>
             </div>
             <p>{artista.bio}</p>
+          </>
+        }
         </div>
     )
 }
